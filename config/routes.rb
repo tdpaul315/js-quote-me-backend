a48670cd840b30a624ac5aeaaea4f10da5702253
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-12-10 15:14:05 -0500

    adds nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/test', to: 'application#test'
   resources :comments
-  resources :quotes
+  resources :quotes do
+    resources :comments 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
